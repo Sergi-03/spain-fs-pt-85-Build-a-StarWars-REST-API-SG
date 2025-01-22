6267c5d7a1c2d6038fd394c84c84039066c7330d
Sergi-03 <robrovskyyt@gmail.com>
2025-01-22 18:23:34 +0000

    base de datos añadida

--- src/models.py
+++ src/models.py
@@ -1,17 +1,49 @@
 from flask_sqlalchemy import SQLAlchemy
-from sqlalchemy.orm import mapped_column
-from sqlalchemy import Integer, String, Boolean
+from sqlalchemy.orm import mapped_column, relationship
+from sqlalchemy import Integer, String, Boolean, Column, ForeignKey
 
 db = SQLAlchemy()
 
 
 class User(db.Model):
-    __tablename__ = "user"
+    __tablename__ = 'user'
+    # Here we define columns for the table person
+    # Notice that each column is also a normal Python instance attribute.
+    id = Column(Integer, primary_key=True)
+    name = Column(String(250), nullable=False, unique=True)
+    email = Column(String(50), nullable=False, unique=True)
+    password = Column(String(50), nullable=False, unique=False)
+    favorites = relationship("Favorite", back_populates="user")
 
-    id = mapped_column(Integer, primary_key=True)
-    email = mapped_column(String(120), nullable=False)
-    password = mapped_column(String(80))
-    is_active = mapped_column(Boolean)
+class Character(db.Model):
+    __tablename__ = 'character'
+    # Here we define columns for the table address.
+    # Notice that each column is also a normal Python instance attribute.
+    id = Column(Integer, primary_key=True)
+    name = Column(String(100), nullable=False, unique=True)
+    description = Column(String(500), nullable=False)
+    favorites = relationship("Favorite", back_populates="character")
+
+class Planet(db.Model):
+    __tablename__ = 'planet'
+    # Here we define columns for the table address.
+    # Notice that each column is also a normal Python instance attribute.
+    id = Column(Integer, primary_key=True)
+    planet_name = Column(String(150), unique=True, nullable=False)
+    description = Column(String(500), nullable=False)
+    favorites = relationship("Favorite", back_populates="planet")
+
+class Favorite(db.Model):
+    __tablename__ = 'favorites'
+    # Here we define columns for the table address.
+    # Notice that each column is also a normal Python instance attribute.
+    id = Column(Integer, primary_key=True)
+    user_id = Column(Integer, ForeignKey('user.id'))
+    character_id = Column(Integer, ForeignKey('character.id'))
+    planet_id = Column(Integer, ForeignKey('planet.id'))
+    user = relationship("User", back_populates="favorites")
+    character = relationship("Character", back_populates="favorites")
+    planet = relationship("Planet", back_populates="favorites")
 
     def __repr__(self):
         return '<User %r>' % self.username
